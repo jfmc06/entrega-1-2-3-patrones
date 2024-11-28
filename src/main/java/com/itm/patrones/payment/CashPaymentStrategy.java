305de0faf4996b11ec4e39eea437b9d4bd311645
Juan Felipe Montoya Correa <juanmontoya1135960@correo.itm.edu.co>
2024-11-28 11:10:33 -0500

    fix

--- src/main/java/com/itm/patrones/payment/CashPaymentStrategy.java
+++ src/main/java/com/itm/patrones/payment/CashPaymentStrategy.java
@@ -5,6 +5,6 @@ import com.itm.patrones.client.Client;
 public class CashPaymentStrategy implements PaymentStrategy {
     @Override
     public void pay(final double amount, final Client client) {
-        System.out.println(client.getName() + " is paying " + amount + " with cash");
+        System.out.println(client.getName() + " will pay " + amount + " in cash");
     }
 }
